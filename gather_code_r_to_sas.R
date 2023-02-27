2bdab13df49adb79ccc83593b3f5819f2dccd291
snehamokashi <77143783+snehamokashi@users.noreply.github.com>
2023-02-27 13:48:12 -0500

    Add files via upload

---
+++ gather_code_r_to_sas.R
@@ -0,0 +1,8 @@
+
+norm_data<-as.data.frame(norm_counts_5dpf_forSAS)
+library(tidyr)
+library(dplyr)
+norm_data_df<-as.data.frame(norm_data)
+norm_data_sas_pmm2<-gather(norm_data_df,key="Sample",value="norm_counts", 2:16)
+View(norm_data_sas_pmm2)
+write.csv(norm_data_sas_pmm2,file="norm_counts_pmm2_sas_2_24_23.csv")
